700ad5ffd04150fc1e9f196157d17fb7e2538842
w227895 <249183617@qq.com>
2020-04-06 21:12:24 +0800

    加入了feign

---
+++ springcloudapi/src/main/java/com/kebo/springcloud/service/DeptService.java
@@ -0,0 +1,30 @@
+package com.kebo.springcloud.service;
+
+import com.kebo.springcloud.po.Dept;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author: kb
+ * @create: 2020-04-06 10:53
+ **/
+@Service
+@FeignClient(value = "SPRINGCLOUDPROVIDERDEPT")
+public interface DeptService {
+
+    @PostMapping("/dept/add")
+    public boolean addDept(Dept dept);
+
+    @GetMapping("/dept/get/{id}")
+    public Dept queryById(@PathVariable("id") long id);
+
+    @GetMapping("/dept/list")
+    public List<Dept> queryAll();
+}
+
